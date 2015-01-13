aa9e8023dc602d7baac9baeb21aa89f64856243a
Rogin <ichaosblade@gmail.com>
2015-01-13 17:09:47 +0800

    Signed-off-by: Rogin <ichaosblade@gmail.com> updated

--- Phonebook/dbo.PersonalInfo.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[PersonalInfo] (
-    [ID]         INT           IDENTITY (1, 1) NOT NULL,
-    [lastName]   NVARCHAR (30) NOT NULL,
-    [firstName]  NVARCHAR (30) NOT NULL,
-    [middleName] NVARCHAR (30) NOT NULL,
-    CONSTRAINT [PK_dbo.PersonalInfo] PRIMARY KEY CLUSTERED ([ID] ASC)
-);
-
